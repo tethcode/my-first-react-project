a597eabdf5a94634b23029bc436323672404075b
tethcode <tethcode@gmail.com>
2025-05-01 20:56:26 +0100

    Completed Project

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
 
+function FactComponent() {
+	return (
+		<div>
+			<h1>Fun facts about React</h1>
+			<ul>
+				<li><b>Was first released in 2013</b></li>
+				<li><b>Was originally created by Jordan Walke</b></li>
+				<li><b>Has well over 100k stars on Github</b></li>
+				<li><b>Is maintained by Meta</b></li>
+				<li><b>Powers thousands of enterprise apps, including mobile apps</b></li>
+			</ul>
+		</div>
+	)
+}
+
 root.render(
-	<ul>
-		<li>Book</li>
-		<li>Pen</li>
-		<li>Bag</li>
-		<li>Cup</li>
-	</ul>
+	<div>
+		<img src="/src/assets/react.svg" id='react-img' alt='React logo'/>
+		<FactComponent />
+	</div>
 )
